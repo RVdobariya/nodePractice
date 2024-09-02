07970cd0d43376b6f1164c0dee9a85ee58ba3c9b
ravidobariya204.rejoice <ravidobariya204.rejoice@gmail.com>
2024-09-02 15:58:12 +0530

    okay

---
+++ utils/apiResponce.js
@@ -0,0 +1,5 @@
+class ApiResponce {
+    constructor(
+        statuscode, data, message = "Success"
+    ) { this.statuscode = statuscode, this.data = data, this.message = message, this.success = statuscode < 400 }
+}
