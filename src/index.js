5f493d9d34ba13adbd7e66d0a34a2ff1199fbe3c
ravidobariya204.rejoice <ravidobariya204.rejoice@gmail.com>
2024-09-02 15:44:23 +0530

    new code added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-import mongoose from "mongoose";
 import dotenv from "dotenv";
 import dbSetup from "../db/index.js";
+import { app } from "./app.js";
+import { asyncHandler } from "../utils/asyncHandler";
 dotenv.config();
 
 
 
-await dbSetup();
+asyncHandler(await dbSetup().then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log("Connected to port", process.env.PORT)
+    })
+}).catch((error) => {
+    console.log("Mongodb Connection Fail")
+}))
+
+
 
 
 
